33ad5e1a9e62a560a80d18c734e4cf8689b77b07
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-05-26 09:02:29 +0700

    remove implicit logCommand call from exec (#653)
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/exec.test.ts
+++ src/exec.test.ts
@@ -1,13 +1,8 @@
-import { beforeEach, describe, expect, test, vi } from "vitest";
+import { describe, expect, test } from "vitest";
 
 import { exec } from "./exec.js";
-import { logCommand } from "./log.js";
-
-vi.mock("./log.js");
 
 const script = "process.stdout.write('out'); process.stderr.write('err')";
-
-beforeEach(() => vi.clearAllMocks());
 
 describe("exec", () => {
   test("rejects on failed command", async () => {
@@ -24,61 +19,41 @@ describe("exec", () => {
 
   test("executes without options", async () => {
     await expect(exec("node", ["-e", script])).resolves.toBeUndefined();
-
-    expect(vi.mocked(logCommand).mock.calls).toStrictEqual([
-      ["node", "-e", script],
-    ]);
   });
 
   test("executes with stdout capture", async () => {
     await expect(
       exec("node", ["-e", script], { stdout: "capture" }),
     ).resolves.toStrictEqual({ stdout: "out" });
-
-    expect(vi.mocked(logCommand)).not.toHaveBeenCalled();
   });
 
   test("executes with stderr capture", async () => {
     await expect(
       exec("node", ["-e", script], { stderr: "capture" }),
     ).resolves.toStrictEqual({ stderr: "err" });
-
-    expect(vi.mocked(logCommand).mock.calls).toStrictEqual([
-      ["node", "-e", script],
-    ]);
   });
 
   test("executes with both capture", async () => {
     await expect(
       exec("node", ["-e", script], { stdout: "capture", stderr: "capture" }),
     ).resolves.toStrictEqual({ stdout: "out", stderr: "err" });
-
-    expect(vi.mocked(logCommand)).not.toHaveBeenCalled();
   });
 
   test("executes with stdout silent", async () => {
     await expect(
       exec("node", ["-e", script], { stdout: "silent" }),
     ).resolves.toBeUndefined();
-
-    expect(vi.mocked(logCommand)).not.toHaveBeenCalled();
   });
 
   test("executes with stderr silent", async () => {
     await expect(
       exec("node", ["-e", script], { stderr: "silent" }),
     ).resolves.toBeUndefined();
-
-    expect(vi.mocked(logCommand).mock.calls).toStrictEqual([
-      ["node", "-e", script],
-    ]);
   });
 
   test("executes with both silent", async () => {
     await expect(
       exec("node", ["-e", script], { stdout: "silent", stderr: "silent" }),
     ).resolves.toBeUndefined();
-
-    expect(vi.mocked(logCommand)).not.toHaveBeenCalled();
   });
 });
